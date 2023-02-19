e083f1fe4a7da6275031004492e11aa133f08044
dheerajq <qqdheeraj@gmail.com>
2023-02-19 18:24:35 +0530

    adding h2 and paragraph using the array.map function

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
+const categories = [
+  {
+    title: "hates",
+    id: "1",
+  },
+  {
+    title: "jackets",
+    id: "1",
+  },
+  {
+    title: "Snekers",
+    id: "3",
+  },
+  {
+    title: "Womens",
+    id: "4",
+  },
+  {
+    title: "Mens",
+    id: "5",
+  },
+];
 const App = () => {
   return (
     <div className="main-container">
       <h1>this is dheeraj</h1>
-      <div className="category-container">
-        <div className="text-container">
-          <h2>hats</h2>
-          <p>shop now</p>
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div className="text-container">
+            <h2>{title}</h2>
+            <p>shop now</p>
+          </div>
         </div>
-      </div>{" "}
-      <div className="category-container">
-        <div className="text-container">
-          <h2>Sneakers</h2>
-          <p>shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <div className="text-container">
-          <h2>Sneakers</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        <div className="text-container">
-          <h2>Womens</h2>
-          <p>shop now</p>
-        </div>
-      </div>{" "}
-      <div className="category-container">
-        <div className="text-container">
-          <h2>Mens</h2>
-          <p>shop now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
