ec52a4dd2502468d9977bf6925abb4cd7948153d
dheerajq <qqdheeraj@gmail.com>
2023-02-19 13:01:27 +0530

    adding-basic text in website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 const App = () => {
-  return <h1>this is dheeraj</h1>;
+  return (
+    <div className="main-container">
+      <h1>this is dheeraj</h1>
+      <div className="category-container">
+        <div className="text-container">
+          <h2>hats</h2>
+          <p>shop now</p>
+        </div>
+      </div>{" "}
+      <div className="category-container">
+        <div className="text-container">
+          <h2>Sneakers</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="text-container">
+          <h2>Sneakers</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="text-container">
+          <h2>Womens</h2>
+          <p>shop now</p>
+        </div>
+      </div>{" "}
+      <div className="category-container">
+        <div className="text-container">
+          <h2>Mens</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
