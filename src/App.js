837e2810143a0c15d3fd4f043279200eb050be6f
dheerajq <qqdheeraj@gmail.com>
2023-02-21 19:20:20 +0530

    learn to use outlet routing element

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import CategoryItem from "./Components/Category-item/Category-item";
-const categories = [
-  {
-    id: 1,
-    title: "hats",
-    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-  },
-  {
-    id: 2,
-    title: "jackets",
-    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-  },
-  {
-    id: 3,
-    title: "sneakers",
-    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-  },
-  {
-    id: 4,
-    title: "womens",
-    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-  },
-  {
-    id: 5,
-    title: "mens",
-    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-  },
-];
+import Home from "./Routers/Home/Home";
+import { Routes, Route } from "react-router-dom";
+const Shop = () => {
+  return <h1> i am the shop page</h1>;
+};
 const App = () => {
   return (
-    <div className="main-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-      {/* <p>dheeraj</p> */}
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
