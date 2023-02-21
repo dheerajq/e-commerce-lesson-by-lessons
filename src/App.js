6779333d1362474e8a26e1d1b94f5e0b6fed5c88
dheerajq <qqdheeraj@gmail.com>
2023-02-21 23:30:18 +0530

    adding sign page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from "./Routers/Home/Home";
 import { Routes, Route } from "react-router-dom";
+import SignIn from "./Routers/sign-in/SignIn";
 import Navigation from "./Routers/Navigation/Navigation";
 const Shop = () => {
   return <h1> i am the shop page</h1>;
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
